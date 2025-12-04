d47a527205d5f9ffbd7f331a687859dcfc937123
Romain Bedouet <romain.flk@gmail.com>
2025-12-04 15:15:42 +0100

    actualisation

--- app/controllers/running_chats_controller.rb
+++ app/controllers/running_chats_controller.rb
@@ -5,12 +5,40 @@ class RunningChatsController < ApplicationController
   end
 
   def create
+    user = current_user
     question = params[:question]
 
-    chat = RubyLLM.chat
-    response = chat.ask("Tu es un expert en running. Réponds uniquement à des questions sur la course à pied. Question: #{question}")
+    profile = "Profil coureur : âge #{user.age}, poids #{user.weight}kg, taille #{user.height}cm, objectif : #{user.goal}, niveau : #{user.level_running}."
 
-    @answer = response.content
+    # Load previous messages
+    history = user.chat_messages.order(:created_at).map do |m|
+      { role: m.role, content: m.content }
+    end
+
+    # Add system message only once (first conversation)
+    if history.empty?
+      history << {
+        role: "system",
+        content: "#{profile} Tu es un coach sportif et professionnel de la course à pieds, tu ne réponds qu'aux questions concernant la course à pied, le running, l'alimentation pour sportifs et bonne pratique du sport en général. "\"Si la question ne concerne ni la course à pieds, ni le running, ni l'alimentation sportive, réponds seulement: 'Je ne répond qu'aux questions concernant le running.'"
+      }
+    end
+
+
+    history << { role: "user", content: question }
+
+    chat = RubyLLM.chat
+    response = chat.ask(history)
+
+    answer = response.content
+
+    user.chat_messages.create(role: "user", content: question)
+    user.chat_messages.create(role: "assistant", content: answer)
+
+    @answer = answer
     render :show
   end
 end
+# CODE A METTRE DANS user_controller.rb
+      # class User < ApplicationRecord
+      #   has_many :chat_messages, dependent: :destroy
+      # end
